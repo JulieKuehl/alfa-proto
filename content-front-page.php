8eb2559ffc37b6fdddc5ff4b422a153891d70ea3
Julie Kuehl <julie@juliekuehl.com>
2015-08-05 12:22:52 -0500

    added front page content area

---
+++ content-front-page.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package Forward
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php forward_featured_image(); ?>
+
+	<header class="entry-header">
+		<h1>FRONT PAGE</h1>
+		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+
+		<?php if ( 'post' == get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php forward_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Continue Reading %s', 'forward' ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			) );
+		?>
+
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'forward' ),
+				'after'  => '</div>',
+				'pagelink' => '<span>%</span>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php forward_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
