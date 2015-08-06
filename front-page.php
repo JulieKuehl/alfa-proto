f44bff579a869bca50a18882100968ae3aaef575
Julie Kuehl <julie@juliekuehl.com>
2015-08-05 19:00:15 -0500

    moved front page slider out of the container

--- front-page.php
+++ front-page.php
@@ -1,24 +1,55 @@
 <?php
 /**
- * The main template file.
+ * The header for our theme.
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ * Displays all of the <head> section and everything up till <div id="content">
  *
  * @package Forward
  */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php wp_head(); ?>
+</head>
 
-get_header(); ?>
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'forward' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="header-container">
+			<div class="site-branding">
+				<h1 class="site-title"><a class="site-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<div class="site-description"><?php bloginfo( 'description' ); ?></div>
+			</div><!-- .site-branding -->
+			<div id="mobile-menu-switch">
+				<a href="" class="toggle">Menu</a>
+			</div><!-- .mobile-menu-switch -->
+		</div><!-- .header-container -->
+	</header><!-- #masthead -->
+
+	<nav id="site-navigation" class="main-navigation" role="navigation">
+		<div class="container">
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => false ) ); ?>
+		</div><!-- .container -->
+	</nav><!-- #site-navigation -->
+
+	<div class="home-page-slider">
+		<?php if ( function_exists( 'soliloquy' ) ) {
+			soliloquy( 'main-home-page-slider', 'slug' );
+		} ?>
+	</div>
+
+	<div id="content" class="site-content">
+		<div class="content-container">
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-			<?php if ( function_exists( 'soliloquy' ) ) {
-				soliloquy( 'main-home-page-slider', 'slug' );
-			} ?>
 
 		<?php if ( have_posts() ) : ?>
 
