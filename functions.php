b541c55b51d2d7d19cf55631013847061276320f
Julie Kuehl <julie@juliekuehl.com>
2015-08-06 16:08:58 -0500

    added woocommerce support

--- functions.php
+++ functions.php
@@ -256,3 +256,25 @@ require get_template_directory() . '/inc/jetpack.php';
  */
 require_once('wp-updates-theme.php');
 new WPUpdatesThemeUpdater_1511( 'http://wp-updates.com/api/2/theme', basename( get_template_directory() ) );
+
+/**
+ * Add WooCommerce support
+ */
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+function my_theme_wrapper_start() {
+	echo '<section id="main">';
+}
+
+function my_theme_wrapper_end() {
+	echo '</section>';
+}
+
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+	add_theme_support( 'woocommerce' );
+}
