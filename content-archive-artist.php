8dcaf76b97b1278a66f10ca1b01d39c8eeecb6bf
Julie Kuehl <julie@juliekuehl.com>
2015-08-17 17:12:50 -0500

    display large thumbnail on artist archive page

--- content-archive-artist.php
+++ content-archive-artist.php
@@ -6,16 +6,17 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php forward_featured_image(); ?>
-
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php forward_posted_on(); ?>
-		</div><!-- .entry-meta -->
+		<?php $image = get_field('artist_photo'); ?>
+		<?php if($image): //dont output an empty image tag ?>
+			<div class="artist-archive-photo">
+				<img src="<?php echo $image['sizes']['large-thumbnail']; ?>" alt="<?php echo $image['caption']; ?>" />
+			</div>
 		<?php endif; ?>
+
+		<?php the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
+
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
