e8b872c68000fed4e6cafd64417c3cc37f504985
Julie Kuehl <julie@juliekuehl.com>
2015-08-18 14:01:45 -0500

    removed a comment

--- content-archive-artist.php
+++ content-archive-artist.php
@@ -11,7 +11,7 @@
 		<div class="artist-archive-photo">
 			<?php
 			$attachment_id = get_field('artist_photo_id');
-			$size = 'large-thumbnail'; // (thumbnail, medium, large, full or custom size)
+			$size = 'large-thumbnail'; 
 			$image = wp_get_attachment_image_src( $attachment_id, $size );
 			$image_url = $image['sizes']['large-thumbnail'];
 			// url = $image[0];
