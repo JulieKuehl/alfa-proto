296c703062672b649a3e16bd54988ed12ec2a85c
Julie Kuehl <julie@juliekuehl.com>
2015-08-19 16:50:31 -0500

    added team member page

---
+++ content-single-team_member.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package Forward
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php forward_featured_image(); ?>
+
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+<!--			--><?php //forward_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+		$attachment_id = get_field('team_member_photo_id');
+		$size = 'large-thumbnail';
+		$image = wp_get_attachment_image_src( $attachment_id, $size );
+		$image_url = $image['sizes']['large-thumbnail'];
+		// url = $image[0];
+		// width = $image[1];
+		// height = $image[2];
+		?>
+
+		<img class="team_member_photo" alt="Image of <?php echo the_title(); ?>" src="<?php echo $image[0]; ?>" />
+
+		<?php the_field( 'team_member_details' ); ?>
+	</div>
+
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'forward' ),
+				'after'  => '</div>',
+				'pagelink' => '<span>%</span>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<div class="entry-social">
+		<?php forward_social_links(); ?>
+	</div>
+
+	<footer class="entry-footer">
+		<?php forward_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->
