52a0d10e1134839240f94567470456559e583cb4
Julie Kuehl <julie@juliekuehl.com>
2015-08-19 15:39:55 -0500

    large-thumbnail on gallery archive page

--- content-archive-gallery.php
+++ content-archive-gallery.php
@@ -12,13 +12,20 @@
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 		<p></p><?php the_field( 'gallery_city' ); ?>, <?php the_field( 'gallery_state' ); ?></p>
 
-		<img src="<?php the_field( 'gallery_photo' ); ?>" height="300" width="auto"/>
+		<div class="artist-archive-photo">
+			<?php
+			$attachment_id = get_field('gallery_photo_id');
+			$size = 'large-thumbnail';
+			$image = wp_get_attachment_image_src( $attachment_id, $size );
+			$image_url = $image['sizes']['large-thumbnail'];
+			// url = $image[0];
+			// width = $image[1];
+			// height = $image[2];
+			?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark">
 
-		<!-- TODO: get medium image to display -->
-		<!-- Code to grab a specified size of the gallery's image -->
-		<?php //$image = wp_get_attachment_image_src(get_field('gallery_photo'), 'full'); ?>
-		<?php //var_dump( $image[0] ); ?>
-<!--		<img src="--><?php //echo $image[0]; ?><!--" alt="--><?php //echo get_the_title(get_field('gallery_photo')) ?><!--" />-->
+				<img class="gallery_photo" alt="Image of <?php echo the_title(); ?>" src="<?php echo $image[0]; ?>" />
+			</a>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 			<div class="entry-meta">
