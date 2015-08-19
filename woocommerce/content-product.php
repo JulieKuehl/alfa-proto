9ce35be42fb425a1740ad65c7ca39c81d5431976
Julie Kuehl <julie@juliekuehl.com>
2015-08-19 13:31:17 -0500

    moved product titles below thumbnail on archive

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -51,19 +51,19 @@ if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] ) {
 		<?php
 
 			/**
-			 * woocommerce_shop_loop_item_title hook
-			 *
-			 * @hooked woocommerce_template_loop_product_title - 10
-			 */
-			do_action( 'woocommerce_shop_loop_item_title' );
-
-			/**
 			 * woocommerce_before_shop_loop_item_title hook
 			 *
 			 * @hooked woocommerce_show_product_loop_sale_flash - 10
 			 * @hooked woocommerce_template_loop_product_thumbnail - 10
 			 */
 			do_action( 'woocommerce_before_shop_loop_item_title' );
+
+			/**
+			 * woocommerce_shop_loop_item_title hook
+			 *
+			 * @hooked woocommerce_template_loop_product_title - 10
+			 */
+			do_action( 'woocommerce_shop_loop_item_title' );
 
 			/**
 			 * woocommerce_after_shop_loop_item_title hook
