d2da7483108a6a4dabf714eea60786b1e2458915
Julie Kuehl <julie@juliekuehl.com>
2015-09-10 15:50:56 -0500

    added content to gallery archive

--- content-archive-gallery.php
+++ content-archive-gallery.php
@@ -10,21 +10,20 @@
 
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-			<p><?php the_field( 'gallery_city' ); ?>, <?php the_field( 'gallery_state' ); ?></p>
 
 		<div class="gallery-archive-photo">
 			<?php
 			$attachment_id = get_field('gallery_photo_id');
-			$size = 'large-thumbnail';
+			$size = 'large-artwork';
 			$image = wp_get_attachment_image_src( $attachment_id, $size );
-			$image_url = $image['sizes']['large-thumbnail'];
+			$image_url = $image['sizes']['large-artwork'];
 			// url = $image[0];
 			// width = $image[1];
 			// height = $image[2];
 			?>
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark">
 
-				<img class="gallery_photo" alt="Image of <?php echo the_title(); ?>" src="<?php echo $image[0]; ?>" />
+				<img alt="Image of <?php echo the_title(); ?>" src="<?php echo $image[0]; ?>" />
 			</a>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
@@ -35,6 +34,15 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+		<?php the_field( 'gallery_address' ); ?><br/>
+		<?php the_field( 'gallery_city'); ?>, <?php the_field( 'gallery_state' ); ?> <?php the_field( 'gallery_zip' ); ?><br/>
+
+		<p><?php the_field( 'gallery_phone' ); ?></p>
+
+		<p>Hours:<br/>
+		<?php the_field( 'gallery_hours' ); ?></p>
+
+		<?php the_field( 'gallery_description' ); ?>
 		<?php
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
